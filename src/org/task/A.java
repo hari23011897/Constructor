d910362c125721101f9f5723ee4fdfe827114f54
hari23011897 <hari23011897@gmail.com>
2022-11-24 15:41:25 +0530

    New

---
+++ src/org/task/A.java
@@ -0,0 +1,14 @@
+package org.task;
+
+public class A extends B {
+	 public A() {
+		 
+		    System.out.println("Default const...");
+		  }
+
+		  public static void main(String[] args) {
+		    A a = new A();
+		  }
+	
+
+}
---
+++ src/org/task/B.java
@@ -0,0 +1,9 @@
+package org.task;
+
+public class B {
+
+		  public B(int id) {
+		    System.out.println(id);
+		  }
+
+}
